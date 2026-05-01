19318ccd5295bf2b3d350950cd800c8b4de271da
vsmislene <vs.mislene@gmail.com>
2026-05-01 17:39:12 -0300

    Update sync_drive.py

--- scripts/sync_drive.py
+++ scripts/sync_drive.py
@@ -1,6 +1,7 @@
 import os
 import io
 import re
+import json
 import unicodedata
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -8,6 +9,7 @@ from googleapiclient.http import MediaIoBaseDownload
 
 FOLDER_ID = "1gHH_jC3MiCpyJjtXGki4vDkzYDhLroj0"
 DEST_FOLDER = "images"
+MAP_FILE = "images_map.json"
 
 def slugify_filename(filename):
     name, ext = os.path.splitext(filename)
@@ -32,12 +34,15 @@ query = f"'{FOLDER_ID}' in parents and trashed = false"
 
 results = service.files().list(
     q=query,
-    fields="files(id, name, mimeType)"
+    fields="files(id, name, mimeType)",
+    pageSize=1000
 ).execute()
 
 files = results.get("files", [])
 
 os.makedirs(DEST_FOLDER, exist_ok=True)
+
+mapa_imagens = {}
 
 for file in files:
     mime = file.get("mimeType", "")
@@ -47,36 +52,26 @@ for file in files:
         continue
 
     file_id = file["id"]
-    filename = slugify_filename(file["name"])
-    filepath = os.path.join(DEST_FOLDER, filename)
+    nome_original = file["name"]
+
+    nome_final = slugify_filename(nome_original)
+    filepath = os.path.join(DEST_FOLDER, nome_final)
 
     request = service.files().get_media(fileId=file_id)
 
     with io.FileIO(filepath, "wb") as fh:
         downloader = MediaIoBaseDownload(fh, request)
         done = False
+
         while not done:
             _, done = downloader.next_chunk()
 
-    print(f"Baixado: {filepath}")
-
-import json
-
-mapa_imagens = {}
-
-for file in files:
-    mime = file.get("mimeType", "")
-
-    if not mime.startswith("image/"):
-        continue
-
-    nome_original = file["name"]
-    nome_final = gerar_nome(nome_original)
-
     nome_sem_ext = os.path.splitext(nome_final)[0]
-
     mapa_imagens[nome_sem_ext] = nome_final
 
-# salva o JSON
-with open("images_map.json", "w") as f:
-    json.dump(mapa_imagens, f)
+    print(f"Baixado: {filepath}")
+
+with open(MAP_FILE, "w", encoding="utf-8") as f:
+    json.dump(mapa_imagens, f, ensure_ascii=False, indent=2, sort_keys=True)
+
+print(f"{MAP_FILE} gerado com {len(mapa_imagens)} imagens.")
